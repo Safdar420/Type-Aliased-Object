c645aae81c30b337e3207b8c13aad91e20c99680
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-05-21 23:06:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var student1 = {
+    name: "SafdarAli",
+    age: 20,
+    CnicNo: 1010101,
+};
+var student2 = {
+    name: "Almas Khan",
+    age: 25,
+    CnicNo: 1017778,
+};
+console.log("you are".concat(student1.age, " year old"));
---
+++ main.ts
@@ -0,0 +1,19 @@
+
+type IStudent ={
+    name:string,
+    age:number,
+    CnicNo:number,
+}
+
+let student1:IStudent ={
+    name:"SafdarAli",
+    age:20,
+    CnicNo:1010101,
+}
+
+let student2:IStudent ={ 
+    name:"Almas Khan",
+    age:25,
+    CnicNo:1017778,
+}
+console.log(`you are ${student1.age} year old`);
